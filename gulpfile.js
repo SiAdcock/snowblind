fad747c6057aec401a6a3fb43a1840019094964d
Simon Adcock <simonadcock@notonthehighstreet.com>
2015-07-04 16:46:16 +0100

    Asset gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,12 @@
 
 var gulp = require('gulp');
 var webpackDevServer = require('./lib/tasks/webpack-dev-server');
+var clean = require('./lib/tasks/clean');
+var js = require('./lib/tasks/js');
 
-gulp.task('browser-sync', browserSync);
-gulp.task('default', ['browser-sync']);
+gulp.task('default', ['server']);
+gulp.task('server', ['assets', 'webpack-dev-server']);
+gulp.task('assets', ['clean', 'js']);
+gulp.task('clean', clean);
+gulp.task('js', js);
 gulp.task('webpack-dev-server', webpackDevServer);
