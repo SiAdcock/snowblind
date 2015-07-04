ba38765cda26b2081e5444e6115b1759d95c3669
Simon Adcock <simonadcock@notonthehighstreet.com>
2015-07-04 17:20:43 +0100

    nodemon and watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 'use strict';
 
 var gulp = require('gulp');
-var webpackDevServer = require('./lib/tasks/webpack-dev-server');
+var nodemon = require('./lib/tasks/nodemon');
 var clean = require('./lib/tasks/clean');
 var js = require('./lib/tasks/js');
+var watch = require('./lib/tasks/watch');
+var webpackDevServer = require('./lib/tasks/webpack-dev-server');
 
 gulp.task('default', ['server']);
-gulp.task('server', ['assets', 'webpack-dev-server']);
+gulp.task('server', ['assets', 'watch', 'webpack-dev-server', 'nodemon']);
 gulp.task('assets', ['clean', 'js']);
 gulp.task('clean', clean);
 gulp.task('js', js);
+gulp.task('watch', watch);
 gulp.task('webpack-dev-server', webpackDevServer);
+gulp.task('nodemon', nodemon);
