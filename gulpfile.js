9571a0a8845216c337997ad6c6491e6eae6e61fb
Simon Adcock <simonadcock@notonthehighstreet.com>
2015-07-04 16:45:17 +0100

    Remove browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var gulp = require('gulp');
-var browserSync = require('./lib/tasks/browser-sync');
 var webpackDevServer = require('./lib/tasks/webpack-dev-server');
 
 gulp.task('browser-sync', browserSync);
