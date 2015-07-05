25fd31bb355b091f3ace103567b409b1ac4c73e0
Simon Adcock <simonadcock@notonthehighstreet.com>
2015-07-05 16:41:41 +0100

    Inst mocha, chai; added test script

---
+++ spec/app/modules/index-spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const expect = require('chai').expect;
+const HTMLComponent = require('../../../app/modules/index');
+
+describe('Index HTML Component', () => {
+  beforeEach(() => {
+
+  });
+  it('renders its markup props', () => {
+    expect(1).to.equal(1);
+  });
+});
