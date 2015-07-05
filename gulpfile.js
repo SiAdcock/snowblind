0a8dfbce65b68dca8f828970bf4f4f482aa6dbd3
Simon Adcock <simonadcock@notonthehighstreet.com>
2015-07-05 11:39:02 +0100

    Lint task and linting

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var clean = require('./lib/tasks/clean');
 var js = require('./lib/tasks/js');
 var watch = require('./lib/tasks/watch');
 var webpackDevServer = require('./lib/tasks/webpack-dev-server');
+var lint = require('./lib/tasks/lint');
 
 gulp.task('default', ['server']);
 gulp.task('server', ['assets', 'watch', 'webpack-dev-server', 'nodemon']);
@@ -13,5 +14,6 @@ gulp.task('assets', ['clean', 'js']);
 gulp.task('clean', clean);
 gulp.task('js', js);
 gulp.task('watch', watch);
+gulp.task('lint', lint);
 gulp.task('webpack-dev-server', webpackDevServer);
 gulp.task('nodemon', nodemon);
