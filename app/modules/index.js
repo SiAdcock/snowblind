d22d4bea5fa54f252f0041bb7699b58784816a3b
Simon Adcock <simonadcock@notonthehighstreet.com>
2015-07-05 11:58:01 +0100

    Only add dev scripts in dev mode

--- app/modules/index.js
+++ app/modules/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
+const env = process.env.NODE_ENV || 'development';
 
 let HtmlComponent = React.createClass({
+  includeScripts: function() {
+    let appSrc;
+    let scriptSrcs = [];
+
+    if (env === 'development') {
+      appSrc = 'http://localhost:8081/build/app.js';
+      scriptSrcs.push('http://localhost:35729/livereload.js');
+    }
+    else {
+      appSrc = 'build/app.js';
+    }
+    scriptSrcs.push(appSrc);
+    return scriptSrcs.map(function(src) {
+      return (
+        <script src={src}/>
+      );
+    });
+  },
   render: function() {
     let state = JSON.stringify(this.props.dehydratedState);
     return (
@@ -17,9 +36,7 @@ let HtmlComponent = React.createClass({
 
       <div id="container"></div>
 
-      {/* TODO: dev server only */}
-      <script src="http://localhost:8081/build/app.js"/>
-      <script src="http://localhost:35729/livereload.js"/>
+      {this.includeScripts()}
       <script dangerouslySetInnerHTML={{__html: 'window.__data = ' + state + ';'}}/>
       </body>
       </html>
