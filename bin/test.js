c857f1df2ac061837952901bef9a2e9490a15beb
Simon Adcock <simonadcock@notonthehighstreet.com>
2015-07-05 16:59:46 +0100

    Test script improvements

---
+++ bin/test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var argv = require('yargs').argv;
+var spawn = require('child_process').spawn;
+var path = require('path');
+var mochaPath = path.join(__dirname, '../node_modules/mocha/bin/mocha');
+var glob = argv._.length ? argv._[0] : 'spec/**/*.js';
+
+spawn(
+  mochaPath,
+  [
+    glob,
+    '--compilers',
+    'js:babel/register'
+  ],
+  {
+    stdio: 'inherit'
+  }
+);
