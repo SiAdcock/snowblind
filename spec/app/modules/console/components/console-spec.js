bb109621886514c7376e9b9c6c1ecefb082b41e7
Simon Adcock <simonadcock@notonthehighstreet.com>
2015-08-31 12:05:14 +0100

    Console component spec

---
+++ spec/app/modules/console/components/console-spec.js
@@ -0,0 +1,37 @@
+'use strict';
+
+import React, { Component } from 'react';
+import createComponent from '../../../../spec-helpers/createComponent';
+
+let Console;
+class OutputMock extends Component {
+  render() {
+    return (<div/>);
+  }
+}
+
+describe('Console component', () => {
+  beforeEach(() => {
+    mockSetup();
+    mockery.registerMock('./output', OutputMock);
+    Console = require('../../../../../app/modules/console/components/console');
+  });
+  afterEach(() => {
+    mockTearDown();
+  });
+  it('renders the console', () => {
+    const logMock = 'foo';
+    const component = createComponent(Console, {log: logMock});
+
+    expect(component.type).to.equal('div');
+    expect(component.props.className).to.equal('console');
+  });
+  it('passes the log to the console output', () => {
+    const logMock = 'foo';
+    const component = createComponent(Console, {log: logMock});
+    const output = component.props.children;
+
+    expect(output.type).to.equal(OutputMock);
+    expect(output.props.log).to.equal(logMock);
+  });
+});
