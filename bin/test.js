4a89b319642a047cea14ade542ed6355df3362fa
Simon Adcock <simonadcock@notonthehighstreet.com>
2015-08-31 11:23:31 +0100

    Scope specs glob to spec directory

--- bin/test.js
+++ bin/test.js
@@ -4,7 +4,7 @@ var argv = require('yargs').argv;
 var spawn = require('child_process').spawn;
 var path = require('path');
 var mochaPath = path.join(__dirname, '../node_modules/mocha/bin/mocha');
-var glob = argv._.length ? argv._[0] : 'spec/**/*.js';
+var glob = argv._.length ? path.join('spec', argv._[0]) : 'spec/**/*.js';
 
 spawn(
   mochaPath,
