62f754cb6c56801fe3830391744c373f50420af0
Simon Adcock <simonadcock@notonthehighstreet.com>
2015-09-06 18:49:21 +0100

    Player move API specs

--- spec/lib/api/player/move-spec.js
+++ spec/lib/api/player/move-spec.js
@@ -2,12 +2,14 @@
 
 const worldMock = {
   DIRECTIONS: {
-    'UP': 'UP'
+    UP: 'UP',
+    DOWN: 'DOWN',
+    LEFT: 'LEFT',
+    RIGHT: 'RIGHT'
   }
 };
 let stateMock;
-let movePlayer;
-let context;
+let movePlayerGenerator;
 
 describe('POST /player/move endpoint', () => {
   beforeEach(() => {
@@ -21,27 +23,80 @@ describe('POST /player/move endpoint', () => {
         speed: 1
       }
     };
-    context = {
+
+    mockery.registerMock('../state', stateMock);
+    mockery.registerMock('../../../app/constants/world', worldMock);
+    movePlayerGenerator = require('../../../../lib/api/player/move');
+  });
+  afterEach(() => {
+    mockTearDown();
+  });
+  it('moves player in the UP direction', () => {
+    let context = {
       request: {
         body: {
           direction: 'UP'
         }
       }
     };
-    mockery.registerMock('../state', stateMock);
-    mockery.registerMock('../../../app/constants/world', worldMock);
-    movePlayer = require('../../../../lib/api/player/move').call(context);
-  });
-  afterEach(() => {
-    mockTearDown();
-  });
-  it('moves player in the expected direction', () => {
     let expectedY = stateMock.player.pos.y - stateMock.player.speed;
+    let movePlayer = movePlayerGenerator.call(context);
     let actualY;
 
     movePlayer.next();
     actualY = JSON.parse(context.body).y;
     expect(actualY).to.equal(expectedY);
     expect(stateMock.player.pos.y).to.equal(expectedY);
+  });
+  it('moves player in the DOWN direction', () => {
+    let context = {
+      request: {
+        body: {
+          direction: 'DOWN'
+        }
+      }
+    };
+    let expectedY = stateMock.player.pos.y + stateMock.player.speed;
+    let movePlayer = movePlayerGenerator.call(context);
+    let actualY;
+
+    movePlayer.next();
+    actualY = JSON.parse(context.body).y;
+    expect(actualY).to.equal(expectedY);
+    expect(stateMock.player.pos.y).to.equal(expectedY);
+  });
+  it('moves player in the LEFT direction', () => {
+    let context = {
+      request: {
+        body: {
+          direction: 'LEFT'
+        }
+      }
+    };
+    let expectedX = stateMock.player.pos.x - stateMock.player.speed;
+    let movePlayer = movePlayerGenerator.call(context);
+    let actualX;
+
+    movePlayer.next();
+    actualX = JSON.parse(context.body).x;
+    expect(actualX).to.equal(expectedX);
+    expect(stateMock.player.pos.x).to.equal(expectedX);
+  });
+  it('moves player in the RIGHT direction', () => {
+    let context = {
+      request: {
+        body: {
+          direction: 'RIGHT'
+        }
+      }
+    };
+    let expectedX = stateMock.player.pos.x + stateMock.player.speed;
+    let movePlayer = movePlayerGenerator.call(context);
+    let actualX;
+
+    movePlayer.next();
+    actualX = JSON.parse(context.body).x;
+    expect(actualX).to.equal(expectedX);
+    expect(stateMock.player.pos.x).to.equal(expectedX);
   });
 });
